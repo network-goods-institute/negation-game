7c1fe2c86842a8e8c676c3cb0b4de0b237370bce
Bikatr7 <Bikatr7@proton.me>
2024-12-07 00:36:03 -0700

    added redirection middleware

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname
+
+  if (pathname === '/dyMiMB') {
+    const url = new URL('/HJSTkZ', request.url)
+    // Return a redirect response
+    return NextResponse.redirect(url)
+  }
+} 
