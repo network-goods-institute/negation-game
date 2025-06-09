ddedcb69e2968bb3804e71d2c62b1ef54f2099d2
Bikatr7 <Bikatr7@proton.me>
2025-06-09 13:09:21 +0900

    reverted src/components/knowledgebase/WhatIsDoubting.tsx

--- src/components/knowledgebase/WhatIsDoubting.tsx
+++ src/components/knowledgebase/WhatIsDoubting.tsx
@@ -4,14 +4,14 @@ export const WhatIsDoubting = () => {
             <h2>What is Doubting?</h2>
             <div className="prose prose-sm dark:prose-invert max-w-none">
                 <p>
-                    &apos;Doubting&apos; is betting against a Restaker&apos;s commitment. When someone
-                    Restakes on a Point-Negation pair, they&apos;re committing to change
-                    their mind if the Negation is valid. Negations can be
-                    counterarguments (arguing the point is incorrect) or objections
-                    (arguing the point is irrelevant). If you Doubt, you&apos;re betting
-                    they won&apos;t follow through. For example, doubt a restake on a
-                    counterargument disproving a fact or an objection claiming it&apos;s
-                    off-topic.
+                    &quot;Doubting&quot; is betting against a Restaker&apos;s commitment. When someone
+                    Restakes on a Point-Negation pair, they commit to changing their mind (Slashing)
+                    if the Negation proves true. If you Doubt their Restake, you&apos;re
+                    essentially saying, &quot;I bet you won&apos;t actually Slash even if you should.&quot;
+                    If the Restaker *does* Slash, you lose your Doubted amount proportionally.
+                    If they *don&apos;t* Slash when the community agrees they should have (a feature
+                    still under development), you win. Doubting incentivizes Restakers to
+                    follow through on their commitments.
                 </p>
                 <p>
                     Doubting reduces the &apos;favor&apos; bonus a Restake provides to its parent
@@ -26,30 +26,11 @@ export const WhatIsDoubting = () => {
                 </p>
             </div>
             <ul>
-                <li>
-                    Doubting costs Cred to place and stakes your spent Cred on the
-                    Restaker&apos;s favor bonus. This applies to restakes on any negation type.
-                </li>
-                <li>
-                    The maximum amount you can doubt is limited by the total effective
-                    favor bonus available on that Point-Negation pair (excluding fully
-                    slashed restakes).
-                </li>
-                <li>
-                    Your earnings accumulate passively from the favor bonus until the
-                    Restaker Slashes, which stops further earnings. Use the &quot;Collect
-                    Earnings&quot; button in your profile to redeem any accumulated earnings.
-                    Make sure to check in on it regularly to collect your earnings or you
-                    may miss out if the Restaker Slashes.
-                </li>
-                <li>
-                    If the Restaker Slashes, your future earnings stop; the Cred you spent
-                    to Doubt is not refunded.
-                </li>
-                <li>
-                    Doubts reduce the favor bonus granted by the Restake immediately upon
-                    placement (using max(slashed, doubted)).
-                </li>
+                <li>Doubting costs Cred to place and stakes your spent Cred on the Restaker&apos;s favor bonus.</li>
+                <li>The maximum amount you can doubt is limited by the total effective favor bonus available on that Point-Negation pair (excluding fully slashed restakes).</li>
+                <li>Your earnings accumulate passively from the favor bonus until the Restaker Slashes, which stops further earnings. Use the &quot;Collect Earnings&quot; button in your profile to redeem any accumulated earnings. Make sure to check in on it regularly to collect your earnings or you may miss out if the Restaker Slashes.</li>
+                <li>If the Restaker Slashes, your future earnings stop; the Cred you spent to Doubt is not refunded.</li>
+                <li>Doubts reduce the favor bonus granted by the Restake immediately upon placement (using max(slashed, doubted)).</li>
             </ul>
         </article>
     );
